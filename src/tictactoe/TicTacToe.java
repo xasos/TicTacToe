cca4e02f682c82b507a8dc0c9f939371f11c7a22
Niraj Pant <nirajpant7@gmail.com>
2014-06-27 13:32:41 -0500

    removed some extra comments

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -2,63 +2,63 @@ package tictactoe;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class TicTacToe 
+public class TicTacToe
 {
     private static final int rows = 3;
     private static final int columns = 3;
     String board[][];
     String r = "";
     String str = "";
-    boolean finalDecision = false; //This will be used to get the final decision
-    
-    
-    
-    public TicTacToe() 
+    boolean finalDecision = false; 
+
+
+
+    public TicTacToe()
     {
         board = new String[rows][columns];
-       
-    for (int i = 0; i < rows; i++) 
+
+    for (int i = 0; i < rows; i++)
     {
-            for (int j = 0; j < columns; j++) 
+            for (int j = 0; j < columns; j++)
             {
                 board[i][j] = " ";
             }
         }
     }
 
-    public void set(int i, int j, String player) 
+    public void set(int i, int j, String player)
     {
-        if (board[i][j].equals(" ")) 
+        if (board[i][j].equals(" "))
         {
             board[i][j] = player;
         }
     }
 
-    public boolean gameOver() 
+    public boolean gameOver()
     {
-        
-        for (int i = 0; i <= 2; i++) 
+
+        for (int i = 0; i <= 2; i++)
         {
-            for (int j = 0; j <= 2; j++) 
+            for (int j = 0; j <= 2; j++)
             {
                 if (board[i][j].equalsIgnoreCase("x") || board[i][j].equalsIgnoreCase("o")) //if the board is full
                 {
                     finalDecision = true;
-                } 
-                
-                else 
+                }
+
+                else
                 {
                     break;
                 }
             }
         }
-        return finalDecision; 
+        return finalDecision;
     }
 
     public boolean winRow() //A method to test if a player wins by a row
     {
         ArrayList<String> list = new ArrayList<>();
-        for (int i = 0; i <= 2; i++) 
+        for (int i = 0; i <= 2; i++)
         {
             for (int j = 0; j <= 2; j++)
             {
@@ -82,7 +82,7 @@ public class TicTacToe
         ArrayList<String> list = new ArrayList<>();
         for (int i = 0; i <= 2; i++)
         {
-            for (int j = 0; j <= 2; j++) 
+            for (int j = 0; j <= 2; j++)
             {
                 if (board[j][i].equalsIgnoreCase("x"))
                 {
@@ -91,7 +91,7 @@ public class TicTacToe
             }
         }
 
-        if (list.size() == 3) 
+        if (list.size() == 3)
         {
             return true;
         }
@@ -99,11 +99,11 @@ public class TicTacToe
         return false;
     }
 
-    public boolean finalWin() 
+    public boolean finalWin()
     {
-        if (gameOver()) 
+        if (gameOver())
         {
-            if (winCol() && winRow()) 
+            if (winCol() && winRow())
             {
                 return true;
             }
@@ -112,16 +112,16 @@ public class TicTacToe
     }
 
     @Override
-    public String toString() 
+    public String toString()
     {
         System.out.println("");
         System.out.println("Valid inputs range from 0,0 to 2,2!");
         System.out.println("");
         str = str + "-------\n";
-        for (int i = 0; i < rows; i++) 
+        for (int i = 0; i < rows; i++)
         {
             r = "|";
-            for (int j = 0; j < columns; j++) 
+            for (int j = 0; j < columns; j++)
             {
                 str = str + r + board[i][j];
             }
@@ -131,5 +131,5 @@ public class TicTacToe
 
         return str;
     }
-    
+
 }
--- src/tictactoe/TicTacToeRunner.java
+++ src/tictactoe/TicTacToeRunner.java
@@ -1,29 +1,28 @@
 package tictactoe;
 import java.util.Scanner;
 
-public class TicTacToeRunner 
+public class TicTacToeRunner
 {
 
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
 
-        Scanner in = new Scanner(System.in); //Instance of the scanner class
-        String player = "x"; //Creates a String variable called player
+        Scanner in = new Scanner(System.in);
+        String player = "x";
         TicTacToe x = new TicTacToe(); //Instance of the class TicTacToe
-        boolean done = false; //Creates a boolean if the game is won, which is set to false
+        boolean done = false;
         while (!done)
         {
             System.out.println(x.toString());
             System.out.println("Row for " + player + " (Type -1 to exit program): ");
             int row = in.nextInt();
-            
-            if (row < 0) 
+
+            if (row < 0)
             {
                 System.out.println("Thanks for playing!");
-                System.exit(0); 
-                
+                System.exit(0);
             }
-            
+
             else
             {
                 System.out.print("Column for " + player + ": ");
@@ -33,25 +32,25 @@ public class TicTacToeRunner
                 {
                     player = "o";
                 }
-                    
+
                 else
                 {
-                  player = "x";  
+                  player = "x";
                 }
-                    
+
                 if(x.finalWin())
                 {
-                     
-                    done = true;
+                  done = true;
                 }
             }
         }
+
         System.out.println(x.toString());
         System.out.println("Congrats, you are the winner!");
         System.out.println("Thanks for playing.");
-        
-        
-        
+
+
+
     }
 
 }
